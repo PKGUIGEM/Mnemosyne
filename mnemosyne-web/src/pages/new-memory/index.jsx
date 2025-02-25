8ca5c76c9c792a8eb6139bd5ef2a53a827df35ec
FIC <fic@info.local>
2025-02-24 21:53:10 -0300

    faltam 4 dias

---
+++ mnemosyne-web/src/pages/new-memory/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header";
+
+const NewMemory = () => {
+
+    return(
+        <>
+            <Header/>
+
+        </>
+    );
+};
+
+export default NewMemory; 
