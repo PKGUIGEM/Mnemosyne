658222929ee34006ba6f4b63e75dadf63f5be8df
FIC <fic@info.local>
2025-02-25 21:56:49 -0300

    subir os dados

--- mnemosyne-web/src/App.jsx
+++ mnemosyne-web/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import Home from "./pages/new-memory";
+import NewMemory from "./pages/new-memory";
 
 function App() {
   return (
